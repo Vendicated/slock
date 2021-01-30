a2aa16aa61bfb0105a94a407be71884ff45d838b
Vendicated <venneptury@protonmail.com>
2021-01-31 00:56:52 +0100

    Patch config

--- config.def.h
+++ config.def.h
@@ -8,5 +8,15 @@ static const char *colorname[NUMCOLS] = {
 	[FAILED] = "#CC3333",   /* wrong password */
 };
 
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
+		{ "color3",       STRING,  &colorname[CAPS] },
+};
+
 /* treat a cleared input like a wrong password (color) */
 static const int failonclear = 1;
